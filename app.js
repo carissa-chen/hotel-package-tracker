e17fdb7ba54af400e54c44739a40fbb80a39556d
jnicolas10 <jsaquicvasquez5278@sdsu.edu>
2026-09-23 21:28:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Enable CORS for front-end requests
 app.use(cors());
 app.use(express.json());
 
-// Routes
 const packageRoutes = require('./routes/packages');
 app.use('/api/packages', packageRoutes);
 
